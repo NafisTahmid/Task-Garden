00388d3df4dff550a3b5c561f3ee60fc30d19ff1
Lenovo <nafistahmid4@gmail.com>
2024-05-23 21:38:31 +0600

    Latest Commit

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCK3jG5sQd8QhUq1jJoax_M_KhGZQLGvAc",
+    authDomain: "taskgarden-ec1a7.firebaseapp.com",
+    projectId: "taskgarden-ec1a7",
+    storageBucket: "taskgarden-ec1a7.appspot.com",
+    messagingSenderId: "1088787689079",
+    appId: "1:1088787689079:web:bc744b79d2ade1aa686072"
+  };
+
+  export default firebaseConfig;
